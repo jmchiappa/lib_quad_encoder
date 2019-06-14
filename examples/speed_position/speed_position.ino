6f6c54d9e475c0f941e5c1a47f62cfb9f9722200
Jean-Marc <jm.chiappa@gmail.com>
2019-06-14 13:31:47 +0200

    Nouvel exemple

--- examples/speed_position/speed_position.ino
+++ examples/speed_position/speed_position.ino
@@ -1,29 +1,77 @@
+#include "transient.h"
 #include "inc_encoder.h"
 
 INC_ENCODER h;
-#define INCREMENT	85 / 800 // 85 mm par tour de 800 pas
+#define INCREMENT	1 //85 / 800 // 85 mm par tour de 800 pas
 
 //uint32_t previous_time=0;
 //uint64_t previous_distance;
 
+#define RISING_EDGE  true
+#define FALLING_EDGE false
+
+#define GEN_CH1 12
+#define GEN_CH2 13
+Transient button;
+
+void setPulse(int chA, int chB) {
+    digitalWrite(chA,HIGH);
+    digitalWrite(chB,LOW);
+    delay(1);
+    digitalWrite(chA,HIGH);
+    digitalWrite(chB,HIGH);
+    delay(1);
+    digitalWrite(chA,LOW);
+    digitalWrite(chB,HIGH);
+    delay(1);
+    digitalWrite(chA,LOW);
+    digitalWrite(chB,LOW);
+    delay(1);
+}
+
+void Pulse(int channel)
+{
+  digitalWrite(13,!digitalRead(13));
+  if(digitalRead(2)){
+    setPulse(GEN_CH1,GEN_CH2);
+  }else{
+    setPulse(GEN_CH2,GEN_CH1);
+  }
+}
+
 void setup() {
+  ;
 	Serial.begin(9600);
-	if(!h.begin(7,8,INCREMENT))
-		Serial.println("Erreur dans l'initialisation");
+  int32_t ret=h.begin(7,8,INCREMENT);
+	if(ret!=0){
+		Serial.print("Erreur dans l'initialisation: ");
+    Serial.println(ret);
+    while(1);
+	}
+  pinMode(13,OUTPUT);
+
+  pinMode(2,INPUT);
+  pinMode(GEN_CH1,OUTPUT);
+  pinMode(GEN_CH2,OUTPUT);
+  button.begin(23 , FALLING_EDGE, INPUT);
+  h.ResetCounter();
 }
 
 void loop() {
 //	uint32_t time = millis();
 //	uint64_t d = h.GetDistance();
 //	if(previous_time!=0) {
-
-		Serial.print("\n\rincr=");
-		Serial.print(h.GetTimerCounter());
-		Serial.print("/tdist=");
-		Serial.print(h.GetDistance());
-		Serial.print("/tspeed=");
-		Serial.print(h.GetSpeed());
-//	}
-//	previous_distance = d;
-//	previous_time = time;
+    if(button.detect()) {
+      Pulse(2);
+  		Serial.print("rincr=");
+  		Serial.print((uint16_t)h.GetTimerCounter());
+  		Serial.print("\tdist=");
+  		Serial.print(h.GetDistance());
+  		Serial.print("\tspeed=");
+  		Serial.println(h.GetSpeed());
+  //	}
+  //	previous_distance = d;
+  //	previous_time = time;
+    }
+    delay(50);
 }
